2ff66663ea2e7a7f08d9689d88d729b7118a1e6c
antokhio <antokhio@gmail.com>
2024-12-20 15:54:10 +0300

    added baseline func, adding help

--- VL.Flex/src/FlexNode.cs
+++ VL.Flex/src/FlexNode.cs
@@ -1,4 +1,5 @@
-﻿using VL.Flex.Internals;
+﻿using System.Runtime.Intrinsics.X86;
+using VL.Flex.Internals;
 using VL.Lib.Collections;
 using YogaSharp;
 using static VL.Flex.Internals.Delegates;
@@ -7,34 +8,65 @@ namespace VL.Flex
 {
     public abstract class FlexBase : IDisposable
     {
-        // Yoga internals
         protected unsafe YGNode* _handle = YGNode.New();
+        /// <summary>
+        /// Node pointer.
+        /// </summary>
         public abstract unsafe YGNode* Handle { get; }
 
-        // Children
         protected Spread<FlexBase?>? _children = null;
+        /// <summary>
+        /// Node children.
+        /// </summary>
         public abstract Spread<FlexBase?>? Children { internal get; set; }
 
-        // Style
         protected IFlexStyle? _style;
+        /// <summary>
+        /// Node style.
+        /// </summary>
         public abstract IFlexStyle? Style { internal get; set; }
 
-        // Calculated Layout
+        #region Layout
         protected FlexLayout _layout;
+        /// <summary>
+        /// Node calculated Layout
+        /// </summary>
         public abstract FlexLayout Layout { get; set; }
+        /// <summary>
+        /// Applies parent layout to node layout
+        /// </summary>
         public abstract void ApplyLayout(FlexLayoutArgs args, FlexLayout? ownerLayout);
-
+        /// <summary>
+        /// Event fired on layout change, used in Breakpoints to conditionally change style.
+        /// </summary>
         public abstract event Action<FlexLayoutArgs>? OnLayoutChanged;
+        /// <summary>
+        /// Node layout should be re-calculated. 
+        /// </summary>
         public abstract bool IsDirty { get; }
+        /// <summary>
+        /// Should apply new layout. 
+        /// </summary>
         public abstract bool HasNewLayout { internal get; set; }
+        /// <summary>
+        /// Calculates layout using this node as a root - upstream. 
+        /// </summary>
         public abstract void CalculateLayout(float? ownerWidth, float? ownerHeight, YGDirection? ownerDirection);
-
-
-        // Owner, so we can traverse to Root from breakpoint (subject to change)
-        protected FlexBase? _owner = null;
+        #endregion
+        /// <summary>
+        /// Used to calculate layout from current node to root, deprecated.
+        /// </summary>
+        #region Owner
+        protected WeakReference<FlexBase>? _owner = null;
+        /// <summary>
+        /// Sets owner.
+        /// </summary>
         public abstract void SetOwner(FlexNode node);
+        /// <summary>
+        /// Traverse to tree root. 
+        /// </summary>
         public abstract FlexBase GetRoot();
-
+        #endregion
 
         /* Measure Func
         *  Since we can pass measure function as static only, 
@@ -45,16 +77,17 @@ namespace VL.Flex
             Store.GetRegistry().AddNode(this);
         }
 
-        // MeasureFunc pointer
-        protected nint _measureFuncPtr;
-
-        // MeasureFunc prop
+        // MeasureFunc
         protected MeasureFunc? _measureFunc;
+        protected nint _measureFuncPtr;
         public abstract MeasureFunc? MeasureFunc { internal get; set; }
-
-
-
         public abstract bool HasMeasureFunc();
+
+        // BaselineFunc
+        protected BaselineFunc? _baselineFunc;
+        protected nint _baselineFuncPtr;
+        public abstract BaselineFunc? BaselineFunc { internal get; set; }
+        public abstract bool HasBaselineFunc();
 
         // Node Context
         // Basically anything we want to pass to measure func.
@@ -197,12 +230,44 @@ namespace VL.Flex
                 }
             }
         }
-
         public override bool HasMeasureFunc()
         {
             unsafe
             {
                 return _handle->HasMeasureFunc();
+            }
+        }
+
+        public override BaselineFunc? BaselineFunc { 
+            internal get => _baselineFunc;
+            set
+            {
+
+                if (_baselineFunc != value)
+                {
+                    if (value != null) {
+                        unsafe
+                        {
+                            _baselineFuncPtr = (nint)(delegate* unmanaged[Cdecl]<YGNode*, float, float, float>)&BaselineFuncAdapter;
+                            Handle->SetBaselineFunc(_baselineFuncPtr);
+                        }
+                    } else
+                    {
+                        unsafe
+                        {
+                            _baselineFuncPtr = 0;
+                            Handle->SetBaselineFunc(_baselineFuncPtr);
+                        }
+                    }
+                }
+            }
+        }
+
+        public override bool HasBaselineFunc()
+        {
+            unsafe
+            {
+                return Handle->HasBaselineFunc();
             }
         }
 
@@ -218,6 +283,9 @@ namespace VL.Flex
             {
                 if (_handle != null)
                 {
+                    _measureFuncPtr = 0;
+                    _baselineFuncPtr = 0;
+
                     Store.GetRegistry().RemoveNode(_handle);
                     _handle->FinalizeNode();
                 }
